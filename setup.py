96950367c47a392670a2070ffc311696ee113394
Semidan <semidan.robaina@gmail.com>
2021-03-12 22:39:19 +0000

    package

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ import importlib
 
 
 def check_package(package):
-    # import importlib
     try:
         importlib.import_module(package)
     except Exception:
@@ -17,16 +16,17 @@ def check_package(package):
 _ = [check_package(p) for p in ['numpy', 'pandas']]
 
 setup(
-    name='brendapy',
+    name='brendapyrser',
     version='0.1.0',
     description='Tools to parse de BRENDA database',
+    # long_description=open('DESCRIPTION.rst').read(),
     url='https://github.com/Robaina/BRENDA_database',
     author='Semidan Robaina Estevez',
     author_email='semidan.robaina@gmail.com',
     license='Creative Commons Attribution 4.0 International',
     packages=find_packages(),
     zip_safe=False,
-    install_requires=['numpy', 'pandas']
-    include_package_data=False, # to include data files in installation
+    install_requires=['numpy', 'pandas'],
+    include_package_data=False,  # to include data files in installation
     # package_data={'': ['data/*.csv']},
 )
