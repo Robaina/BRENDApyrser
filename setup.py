c61e5576f8f77ab2d7934fe52aa6a38b1286c9e3
Semidan <semidan.robaina@gmail.com>
2021-07-25 19:24:14 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-from setuptools import find_packages, setup
-import importlib
+from setuptools import setup  find_packages,
+# import importlib
 
 
-def check_package(package):
-    try:
-        importlib.import_module(package)
-    except Exception:
-        raise ImportError("Requires {package} to "
-                          "be installed before running setup.py (pip install {package})"
-                          .format(package=package))
-    finally:
-        pass
-
-
-_ = [check_package(p) for p in ['numpy', 'pandas']]
+# def check_package(package):
+#     try:
+#         importlib.import_module(package)
+#     except Exception:
+#         raise ImportError("Requires {package} to "
+#                           "be installed before running setup.py (pip install {package})"
+#                           .format(package=package))
+#     finally:
+#         pass
+#
+#
+# _ = [check_package(p) for p in ['numpy', 'pandas']]
 
 setup(
     name='brendapyrser',
@@ -24,9 +24,9 @@ setup(
     author='Semidan Robaina Estevez',
     author_email='semidan.robaina@gmail.com',
     license='Creative Commons Attribution 4.0 International',
-    packages=find_packages(),
+    # packages=find_packages(),
     zip_safe=False,
     install_requires=['numpy', 'pandas'],
-    include_package_data=False,  # to include data files in installation
+    # include_package_data=False,  # to include data files in installation
     # package_data={'': ['data/*.csv']},
 )
